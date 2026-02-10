fb8730e2b4a5f2b3903259d1d49dc199772449b2
aldairGG2315 <aldaircoyolt@gmail.com>
2026-02-10 03:54:52 -0500

    asd

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Landing-jetbrains-styles/',
+  base: '/Landing-jetbrains-style/',
 
 })
